2b0740ba02818a59ff3a15974a537b3e3e8696b9
seokhoonj <seokhoonj@gmail.com>
2022-07-28 20:01:23 +0900

    get_prop functions are added

--- R/summary.R
+++ R/summary.R
@@ -35,7 +35,7 @@ prop_table <- function(x, digits = 2) {
   v <- unilen(x)
   if (v >= 1000)
     stop("Distinct values >= 100")
-  round(prop.table(table(x)) * 100, digits = digits)
+  round(prop.table(table(x, useNA = "ifany")) * 100, digits = digits)
 }
 
 group_stats <- function(df, group_var, value_var, fun.aggregate = sum) {
@@ -48,4 +48,73 @@ group_stats <- function(df, group_var, value_var, fun.aggregate = sum) {
 group_stats_ <- function(df, group_var, value_var, fun.aggregate = sum) {
   assert_class(df, "data.table")
   df[, lapply(.SD, fun.aggregate), keyby = group_var, .SDcols = value_var]
+}
+
+get_prop <- function(df, group_var, uniq_var, sum_var, multiple = 100) {                                                                                group_var <- match_cols(df, vapply(substitute(group_var), deparse, "character"))
+  assert_class(df, "data.table")
+  if (!missing(uniq_var)) {
+    uniq_var <- match_cols(df, vapply(substitute(uniq_var), deparse, "character"))
+    if (!missing(sum_var)) {
+      sum_var <- match_cols(df, vapply(substitute(sum_var), deparse, "character"))
+      z <- df[, .(n = .N,
+                  uniq_n = uniqueN(get(uniq_var)),
+                  sum = sum(get(sum_var)),
+                  sum_per_uniq_n = sum(get(sum_var)) / uniqueN(get(uniq_var))),
+              by = group_var]
+      set(z, j = "n_prop"     , value = z$n      / sum(z$n)      * multiple)
+      set(z, j = "uniq_n_prop", value = z$uniq_n / uniqueN(df[[uniq_var]]) * multiple)
+      set(z, j = "sum_prop"   , value = z$sum    / sum(z$sum)    * multiple)
+    } else {
+      z <- df[, .(n = .N,
+                  uniq_n = uniqueN(get(uniq_var))),
+              by = group_var]
+      set(z, j = "n_prop"     , value = z$n      / sum(z$n)      * multiple)
+      set(z, j = "uniq_n_prop", value = z$uniq_n / uniqueN(df[[uniq_var]]) * multiple)
+    }
+  } else {
+    if (!missing(sum_var)) {
+      sum_var <- match_cols(df, vapply(substitute(sum_var), deparse, "character"))
+      z <- df[, .(n = .N, sum = sum(get(sum_var))), by = group_var]
+      set(z, j = "n_prop"     , value = z$n      / sum(z$n)      * multiple)
+      set(z, j = "sum_prop"   , value = z$sum    / sum(z$sum)    * multiple)
+    } else {
+      z <- df[, .(n = .N), by = group_var]
+      set(z, j = "prop", value = z$n / sum(z$n) * multiple)
+    }
+  }
+  setorderv(z, group_var)
+  return(z)
+}
+
+get_prop_ <- function(df, group_var, uniq_var, sumivar, multiple = 100) {
+  group_var <- match_cols(df, group_var)
+  if (!missing(uniq_var)) {
+    if (!missing(sum_var)) {
+      z <- df[, .(n = .N,
+                  uniq_n = uniqueN(get(uniq_var)),
+                  sum = sum(get(sum_var)),
+                  sum_per_uniq_n = sum(get(sum_var)) / uniqueN(get(uniq_var))),
+              by = group_var]
+      set(z, j = "n_prop"     , value = z$n      / sum(z$n)      * multiple)
+      set(z, j = "uniq_n_prop", value = z$uniq_n / uniqueN(df[[uniq_var]]) * multiple)
+      set(z, j = "sum_prop"   , value = z$sum    / sum(z$sum)    * multiple)
+    } else {
+      z <- df[, .(n = .N,
+                  uniq_n = uniqueN(get(uniq_var))),
+              by = group_var]
+      set(z, j = "n_prop"     , value = z$n      / sum(z$n)      * multiple)
+      set(z, j = "uniq_n_prop", value = z$uniq_n / uniqueN(df[[uniq_var]]) * multiple)
+    }
+  } else {
+    if (!missing(sum_var)) {
+      z <- df[, .(n = .N, sum = sum(get(sum_var))), by = group_var]
+      set(z, j = "n_prop"     , value = z$n      / sum(z$n)      * multiple)
+      set(z, j = "sum_prop"   , value = z$sum    / sum(z$sum)    * multiple)
+    } else {
+      z <- df[, .(n = .N), by = group_var]
+      set(z, j = "prop", value = z$n / sum(z$n) * multiple)
+    }
+  }
+  setorderv(z, group_var)
+  return(z)
 }
