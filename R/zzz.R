13d76903cc47530c2f8d8c572cf0f7d866d376bb
seokhoonj <seokhoonj@gmail.com>
2022-07-30 13:44:55 +0900

    add import

--- R/zzz.R
+++ R/zzz.R
@@ -5,7 +5,9 @@
   options(vuw.eps = 1e-8)
   options(vuw.triple.colors = c("#E41A1C", "#377EB8", "#4DAF4A"))
   options(vuw.gender.colors = c("#00BFC4", "#F8766D"))
-  source_python(
+  reticulate::import("numpy")
+  reticulate::import("pandas")
+  reticulate::source_python(
     paste(system.file(package = "vuw"), "python/jap2kor.py", sep = "/"),
     envir = globalenv()
   )
