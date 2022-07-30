58b69e37644059557074904d080731ba203c0459
seokhoonj <seokhoonj@gmail.com>
2022-07-30 12:38:52 +0900

    language functions are all modified

--- R/zzz.R
+++ R/zzz.R
@@ -5,7 +5,10 @@
   options(vuw.eps = 1e-8)
   options(vuw.triple.colors = c("#E41A1C", "#377EB8", "#4DAF4A"))
   options(vuw.gender.colors = c("#00BFC4", "#F8766D"))
-  source_python(paste(system.file(package = "vuw"), "python/jap2kor.py", sep = "/"))
+  source_python(
+    paste(system.file(package = "vuw"), "python/jap2kor.py", sep = "/"),
+    envir = parent.frame()
+  )
   if (Sys.info()[["sysname"]] == "Linux")
     Sys.setlocale("LC_MESSAGES", "en_US.UTF-8")
 }
