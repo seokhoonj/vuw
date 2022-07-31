82c33d83167667fc8f4a8ddbb9c46c2bd871fde3
seokhoonj <seokhoonj@gmail.com>
2022-07-31 12:49:47 +0900

    theme functions are added

--- R/style.R
+++ R/style.R
@@ -3,24 +3,24 @@ pad  <- function(x, width =  2) str_pad(x, width = width, pad = "0")
 pad0 <- function(x, width = 14) str_pad(x, width = width, pad = " ")
 decimal <- function(x, digit = 1) sprintf(paste0("%.", digit, "f"), x)
 
-theme_view <- function(x.angle = 0) {
+theme_view <- function(family = "Cascade Code", x.angle = 0, y.angle = 0) {
   theme(
-    text  = element_text(family = "Malgun Gothic"),
-    title = element_text(family = "Malgun Gothic"),
+    text  = element_text(family = family),
+    title = element_text(family = family),
     strip.text.x = element_text(face = "bold"),
     axis.text.x  = element_text(face = "bold", angle = x.angle),
-    axis.text.y  = element_text(face = "bold"),
+    axis.text.y  = element_text(face = "bold", angle = y.angle),
     legend.position = "bottom"
   )
 }
 
-theme_save <- function(x.angle = 0) {
+theme_save <- function(family = "Cascade Code", x.angle = 0, y.angle = 0) {
   theme(
-    text  = element_text(family = "Malgun Gothic"),
-    title = element_text(family = "Malgun Gothic"),
+    text  = element_text(family = family),
+    title = element_text(family = family),
     strip.text.x = element_text(size = 17, face = "bold"),
     axis.text.x  = element_text(size = 12, face = "bold", angle = x.angle),
-    axis.text.y  = element_text(size = 12, face = "bold"),
+    axis.text.y  = element_text(size = 12, face = "bold", angle = y.angle),
     legend.position = "bottom"
   )
 }
