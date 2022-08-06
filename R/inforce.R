47b52c04f2f343b2efcf4c2e7d4f5cc2a5cb455d
seokhoonj <seokhoonj@gmail.com>
2022-08-06 18:24:48 +0900

    get_period_inforce function is modified

--- R/inforce.R
+++ R/inforce.R
@@ -1,16 +1,18 @@
 
-get_period_inforce <- function(df, policy_var, policy_date, last_date, group = 1L) {
-  policy_var  <- deparse(substitute(policy_var))
-  policy_date <- deparse(substitute(policy_date))
-  last_date   <- deparse(substitute(last_date))
+get_period_inforce <- function(df, group_var, policy_var, policy_date, last_date, group = 1L) {
+  group_var   <- match_cols(df, vapply(institute(group_var), deparse, "character"))
+  policy_var  <- match_cols(df, deparse(substitute(policy_var)))
+  policy_date <- match_cols(df, deparse(substitute(policy_date)))
+  last_date   <- match_cols(df, deparse(substitute(last_date)))
   p <- diff_period(df[[policy_date]], df[[last_date]])
-  df[, period := p]
-  inforce <- df[, .(n = uniqueN(get(policy_var))), keyby = .(period)][order(-period)]
-  inforce[, n := cumsum(n)]
+  df[, `:=`(period, p)]
+  group_cols <-  c("period", group_var)
+  inforce <- df[, .(n = uniqueN(get(policy_var))), keyby = group_cols][order(-period)]
+  inforce[, `:=`(n, cumsum(n)), group_var]
   inforce <- inforce[order(period)]
   if (group > 1) {
-    inforce[, period := (period-1) %/% group + 1]
-    return(inforce[, .(n = mean(n)), .(period)])
+    inforce[, `:=`(period, (period - 1)%/%group + 1)]
+    return(inforce[, .(n = mean(n)), group_cols])
   }
   return(inforce)
 }
