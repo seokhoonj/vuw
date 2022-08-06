5e1ab5e3cc8eef426bfe97a567947c66b4bd6670
seokhoonj <seokhoonj@gmail.com>
2022-08-06 21:15:31 +0900

    get_period_inforce function is modified

--- R/inforce.R
+++ R/inforce.R
@@ -10,9 +10,18 @@ get_period_inforce <- function(df, group_var, policy_var, policy_date, last_date
   inforce <- df[, .(n = uniqueN(get(policy_var))), keyby = group_cols][order(-period)]
   inforce[, `:=`(n, cumsum(n)), group_var]
   inforce <- inforce[order(period)]
-  if (group > 1) {
-    inforce[, `:=`(period, (period - 1)%/%group + 1)]
-    return(inforce[, .(n = mean(n)), group_cols])
+  # inforce are added if there is no policy with only 1st period
+  inforce_add <- inforce[, .(period = min(period)-1, n = max(n)), keyby = group_var][period > 0]
+  if (nrow(inforce_add) > 0) {
+    inforce_add <- reprow(inforce_add, inforce_add$period)
+    inforce_add[, `:=`(period, rank(period, ties.method = "first")), keyby = group_var]
+    inforce <- rbind(inforce_add, inforce)
+    setorderv(inforce, group_cols)
   }
-  return(inforce)
+  if (group > 1) {
+    inforce[, `:=`(period, (period-1) %/% group + 1)]
+    return(inforce[, .(n = mean(n)), keyby = group_cols])
+  }
+  return(inforce[])
 }
+
