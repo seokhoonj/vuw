b3d827d0034e641e48db5646fc6ebd8a8d3f8d78
seokhoonj <seokhoonj@gmail.com>
2022-08-08 10:00:20 +0900

    last <- latest

--- R/inforce.R
+++ R/inforce.R
@@ -1,10 +1,10 @@
 
-get_period_inforce <- function(df, group_var, policy_var, policy_date, last_date, group = 1L) {
+get_period_inforce <- function(df, group_var, policy_var, policy_date, latest_date, group = 1L) {
   group_var   <- match_cols(df, vapply(substitute(group_var), deparse, "character"))
   policy_var  <- match_cols(df, deparse(substitute(policy_var)))
   policy_date <- match_cols(df, deparse(substitute(policy_date)))
-  last_date   <- match_cols(df, deparse(substitute(last_date)))
-  p <- diff_period(df[[policy_date]], df[[last_date]])
+  latest_date <- match_cols(df, deparse(substitute(latest_date)))
+  p <- diff_period(df[[policy_date]], df[[latest_date]])
   df[, `:=`(period, p)]
   group_cols <-  c("period", group_var)
   inforce <- df[, .(n = uniqueN(get(policy_var))), keyby = group_cols][order(-period)]
