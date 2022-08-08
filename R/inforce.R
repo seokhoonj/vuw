c1eeac63af10d4acf48fa7484012f8e30e41f671
seokhoonj <seokhoonj@gmail.com>
2022-08-08 11:06:40 +0900

    get_period_inforce_by_policy_ym

--- R/inforce.R
+++ R/inforce.R
@@ -25,3 +25,20 @@ get_period_inforce <- function(df, group_var, policy_var, policy_date, latest_da
   return(inforce[])
 }
 
+get_period_inforce_by_policy_ym <- function(df, group_var, policy_var, policy_ym, latest_ym, group = 1L) {
+  group_var <- match_cols(df, vapply(substitute(group_var),
+                                     deparse, "character"))
+  policy_var <- match_cols(df, deparse(substitute(policy_var)))
+  policy_ym <- match_cols(df, deparse(substitute(policy_ym)))
+  latest_ym <- match_cols(df, deparse(substitute(latest_ym)))
+  group_cols <- c(group_var, policy_ym, latest_ym)
+  dm <- df[, .(n = uniqueN(policy)), keyby = group_cols]
+  ym_list <- vseq(dm[[policy_ym]], dm[[latest_ym]], by = "month")
+  times <- sapply(ym_list, length)
+  z <- reprow(dm, times)
+  z[, inforce_ym := num2date(unlist(ym_list))]
+  p <- diff_period(z[[policy_ym]], z[["inforce_ym"]], group = group)
+  z[, period := p]
+  group_cols <- c(group_var, "inforce_ym", "period")
+  z[, .(n = sum(n)), keyby = group_cols]
+}
