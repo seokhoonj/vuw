5d44c1acf332b551be80fd19a2d3ef375bbf9979
seokhoonj <seokhoonj@gmail.com>
2022-09-29 13:14:08 +0900

    set_lr function is modified

--- R/rp.R
+++ R/rp.R
@@ -278,15 +278,15 @@ apply_weight <- function(df, weight) {
 }
 
 set_lr <- function(df) {
-  loss_cols <- regmatch_cols(dm, "^loss[0-9_]")
-  loss_type <- gsub("^loss_", "", loss_cols)
-  rp_cols   <- regmatch_cols(dm, "^rp[0-9_]")
-  rp_type   <- gsub("^rp_", "", rp_cols)
+  loss_cols <- regmatch_cols(dm, "^loss|")
+  loss_type <- gsub("^loss", "", loss_cols)
+  rp_cols   <- regmatch_cols(dm, "^rp")
+  rp_type   <- gsub("^rp", "", rp_cols)
 
   lr_type   <- intersect(loss_type, lr_type)
-  lr_cols   <- sprintf("lr_%s"  , lr_type)
-  rp_cols   <- sprintf("rp_%s"  , lr_type)
-  loss_cols <- sprintf("loss_%s", lr_type)
+  lr_cols   <- sprintf("lr%s"  , lr_type)
+  rp_cols   <- sprintf("rp%s"  , lr_type)
+  loss_cols <- sprintf("loss%s", lr_type)
 
   for (i in seq_along(lr_cols)) {
     set(df, j = lr_cols[i], value = df[[loss_cols[i]]] / df[[rp_cols[i]]])
