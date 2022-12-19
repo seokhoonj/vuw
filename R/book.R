78d397664334989b57497611dc208f4d773ecd14
seokhoonj <seokhoonj@gmail.com>
2022-12-19 16:05:55 +0900

    label is added in risk_plot function

--- R/book.R
+++ R/book.R
@@ -131,6 +131,7 @@ risk_plot <- function(risk_info, x, nrow = NULL, ncol = NULL, scales = "free_y",
   if (logscale) {
     g <- ggplot(risk_info, aes(x = age, y = log(rate), group = gender, col = gender)) +
       geom_line() +
+      geom_text(aes(label = label), vjust = -.25)
       scale_gender_manual(risk_info$gender) +
       scale_x_continuous(n.breaks = floor(unilen(risk_info$age) / age_unit)) +
       scale_y_continuous(labels = function(x) sprintf("%.2f%%", exp(x) * 100)) +
@@ -138,6 +139,7 @@ risk_plot <- function(risk_info, x, nrow = NULL, ncol = NULL, scales = "free_y",
   } else {
     g <- ggplot(risk_info, aes(x = age, y = rate, group = gender, col = gender)) +
       geom_line() +
+      geom_text(aes(label = label), vjust = -.25)
       scale_gender_manual(risk_info$gender) +
       scale_x_continuous(n.breaks = floor(unilen(risk_info$age) / age_unit)) +
       scale_y_continuous(labels = function(x) sprintf("%.2f%%", x * 100)) +
