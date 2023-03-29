66e357857bdd0fd5597bcbadae474564e7d8d2da
seokhoonj <seokhoonj@gmail.com>
2023-03-29 17:30:54 +0900

    style functions are modified

--- R/style.R
+++ R/style.R
@@ -4,7 +4,8 @@ pad0 <- function(x, width = 14) str_pad(x, width = width, pad = " ")
 decimal <- function(x, digit = 1) sprintf(paste0("%.", digit, "f"), x)
 
 theme_view <- function(family = "Malgun Gothic",
-                       x.size  = NULL, y.size  = NULL, t.size = NULL, s.size = NULL,
+                       x.size  = NULL  , y.size = NULL, t.size = NULL  , s.size = NULL,
+                       x.face  = "bold", y.face = NULL, t.face = "bold", s.face = "bold",
                        x.angle =    0, y.angle =    0, # y.comma = TRUE,
                        x.hjust =   .5, x.vjust =   .5,
                        y.hjust =   .5, y.vjust =   .5,
@@ -12,10 +13,10 @@ theme_view <- function(family = "Malgun Gothic",
   list(
     theme(
       text  = element_text(family = family),
-      title = element_text(family = family, size = t.size, face = "bold"),
-      strip.text.x = element_text(size = s.size, face = "bold"),
-      axis.text.x  = element_text(size = x.size, face = "bold", angle = x.angle, hjust = x.hjust, vjust = x.vjust),
-      axis.text.y  = element_text(size = y.size, face = "bold", angle = y.angle, hjust = y.hjust, vjust = y.vjust),
+      title = element_text(family = family, size = t.size, face = t.face),
+      strip.text.x = element_text(size = s.size, face = s.face),
+      axis.text.x  = element_text(size = x.size, face = x.face, angle = x.angle, hjust = x.hjust, vjust = x.vjust),
+      axis.text.y  = element_text(size = y.size, face = y.face, angle = y.angle, hjust = y.hjust, vjust = y.vjust),
       legend.position = legend.position
     )
     # if (y.comma) scale_y_continuous(labels = comma)
@@ -23,7 +24,8 @@ theme_view <- function(family = "Malgun Gothic",
 }
 
 theme_shiny <- function(family = "Malgun Gothic",
-                        x.size  = 12, y.size  = 12, t.size = 17, s.size = 17,
+                        x.size  = 12    , y.size = 12  , t.size = 17    , s.size = 17,
+                        x.face  = "bold", y.face = NULL, t.face = "bold", s.face = "bold",
                         x.angle =  0, y.angle =  0, # y.comma = TRUE,
                         x.hjust = .5, x.vjust = .5,
                         y.hjust = .5, y.vjust = .5,
@@ -31,10 +33,10 @@ theme_shiny <- function(family = "Malgun Gothic",
   list(
     theme(
       text  = element_text(family = family),
-      title = element_text(family = family, size = t.size, face = "bold"),
-      strip.text.x = element_text(size = s.size, face = "bold"),
-      axis.text.x  = element_text(size = x.size, face = "bold", angle = x.angle, hjust = x.hjust, vjust = x.vjust),
-      axis.text.y  = element_text(size = y.size, face = "bold", angle = y.angle, hjust = y.hjust, vjust = y.vjust),
+      title = element_text(family = family, size = t.size, face = t.face),
+      strip.text.x = element_text(size = s.size, face = s.face),
+      axis.text.x  = element_text(size = x.size, face = x.face, angle = x.angle, hjust = x.hjust, vjust = x.vjust),
+      axis.text.y  = element_text(size = y.size, face = y.face, angle = y.angle, hjust = y.hjust, vjust = y.vjust),
       legend.position = legend.position
     )
     # if (y.comma) scale_y_continuous(labels = comma)
@@ -42,7 +44,8 @@ theme_shiny <- function(family = "Malgun Gothic",
 }
 
 theme_save <- function(family = "Malgun Gothic",
-                       x.size  = 12, y.size  = 12, t.size = 14, s.size = 14,
+                       x.size  = 12    , y.size = 12  , t.size = 14    , s.size = 14,
+                       x.face  = "bold", y.face = NULL, t.face = "bold", s.face = "bold",
                        x.angle =  0, y.angle =  0, # y.comma = TRUE,
                        x.hjust = .5, x.vjust = .5,
                        y.hjust = .5, y.vjust = .5,
@@ -50,10 +53,10 @@ theme_save <- function(family = "Malgun Gothic",
   list(
     theme(
       text  = element_text(family = family),
-      title = element_text(family = family, size = t.size, face = "bold"),
-      strip.text.x = element_text(size = s.size, face = "bold"),
-      axis.text.x  = element_text(size = x.size, face = "bold", angle = x.angle, hjust = x.hjust, vjust = x.vjust),
-      axis.text.y  = element_text(size = y.size, face = "bold", angle = y.angle, hjust = y.hjust, vjust = y.vjust),
+      title = element_text(family = family, size = t.size, face = t.face),
+      strip.text.x = element_text(size = s.size, face = s.face),
+      axis.text.x  = element_text(size = x.size, face = x.face, angle = x.angle, hjust = x.hjust, vjust = x.vjust),
+      axis.text.y  = element_text(size = y.size, face = y.face, angle = y.angle, hjust = y.hjust, vjust = y.vjust),
       legend.position = legend.position
     )
     # if (y.comma) scale_y_continuous(labels = comma)
