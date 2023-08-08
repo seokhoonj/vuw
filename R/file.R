581bdf15a5638831dbbc7d33ddf30f89ce5786c6
seokhoonj <seokhoonj@gmail.com>
2023-08-08 18:19:26 +0900

    create_library_r function is added

---
+++ R/file.R
@@ -0,0 +1,31 @@
+
+create_library_r <- function(file = "library.R", path) {
+  if (!missing(path))
+    file <- paste0(path, "/", file)
+  file.create(file)
+  libs <- c(
+    "bslib",
+    "data.table",
+    "devtools",
+    "ecos",
+    "gridExtra",
+    "kosis",
+    "openxlsx",
+    "plotly",
+    "readxl",
+    "rintrojs",
+    "shiny",
+    "shinyBS",
+    "shinycssloaders",
+    "shinydashboard",
+    "shinyjs",
+    "shinyWidgets",
+    "tibble",
+    "vuw"
+  )
+  cat(
+    "\nsuppressPackageStartupMessages({",
+    paste0("\n\tlibrary(", libs, ")"), "\r})",
+    file = file
+  )
+}
