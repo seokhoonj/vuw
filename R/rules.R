6f692b45489d6cc454b8a5aa375b7ab0d2ab0976
seokhoonj <seokhoonj@gmail.com>
2023-08-11 22:00:53 +0900

    apply_rules are modified

--- R/rules.R
+++ R/rules.R
@@ -1,27 +1,51 @@
 
+# apply_rules <- function(rule_info, df) {
+#   dm <- merge(df, rule_info, by = c("kcd"), all.x = TRUE, allow.cartesian = TRUE)
+#   dm[!is.na(kcd) & age >= age_min & age <= age_max & hos >= hos_min & hos <= hos_max & sur >= sur_min & sur <= sur_max & elp >= elp_min,
+#      order_result := result]
+#   dm[!is.na(kcd) & is.na(order), `:=`(order = 0, order_result = "na")]
+#   dm[!is.na(kcd) & is.na(order_result), `:=`(order_result = "dec")]
+#   do <- dm[!is.na(order_result), .(order = min(order)), .(id, kcd)]
+#   dm[do, on = .(id, kcd, order), interim_result := order_result]
+#   dm[is.na(kcd), `:=`(order = 0, interim_result = "std")]
+#   z <- dm[!is.na(interim_result)]
+#   z[, final_result := sort_code(order_result), .(id)]
+#   z[final_result == "", final_result := "std"]
+#   rm_cols(z, .(result, order_result))
+#   setorder(z, id, final_result)
+#   z[grepl("dec", final_result, ignore.case = TRUE),
+#     decision := "dec"]
+#   z[is.na(decision) & grepl("na|uwer", final_result, ignore.case = TRUE),
+#     decision := "uwer"]
+#   z[is.na(decision) & grepl("excl", final_result, ignore.case = TRUE),
+#     decision := pull_code_all("excl[0-9]\\([0-9\\-\\|\\[\\]]+\\)", final_result,
+#                               collapse = ",", ignore.case = TRUE)]
+#   set(z, i = which(is.na(z$decision)), j = "decision", value = "std")
+#   set(z, j = "decision", value = toupper(z$decision))
+#   return(z[])
+# }
+
 apply_rules <- function(rule_info, df) {
-  dm <- merge(df, rule_info, by = c("kcd"), all.x = TRUE, allow.cartesian = TRUE)
-  dm[!is.na(kcd) & age >= age_min & age <= age_max & hos >= hos_min & hos <= hos_max & sur >= sur_min & sur <= sur_max & elp >= elp_min,
-     order_result := result]
-  dm[!is.na(kcd) & is.na(order), `:=`(order = 0, order_result = "na")]
-  dm[!is.na(kcd) & is.na(order_result), `:=`(order_result = "dec")]
-  do <- dm[!is.na(order_result), .(order = min(order)), .(id, kcd)]
-  dm[do, on = .(id, kcd, order), interim_result := order_result]
-  dm[is.na(kcd), `:=`(order = 0, interim_result = "std")]
+  dm <- rule_info[df, on = .(kcd), allow.cartesian = TRUE]
+  colorder <- unique(c(names(df), names(rule_info)))
+  setcolorder(dm, colorder)
+  dm[!is.na(kcd) &
+       age >= age_min & age <= age_max &
+       hos >= hos_min & hos <= hos_max &
+       sur >= sur_min & sur <= sur_max &
+       elp >= elp_min & elp <= elp_max, `:=`(order_result, result)]
+  dm[is.na(kcd), `:=`(order = 0, order_result = "std")]
+  dm[!is.na(kcd) & is.na(order_result), `:=`(order = 0, order_result = "na")]
+  do <- dm[order_result != "na", .(order = min(order)), .(id, kcd)]
+  dm[do, `:=`(interim_result, order_result), on = .(id, kcd, order)]
   z <- dm[!is.na(interim_result)]
-  z[, final_result := sort_code(order_result), .(id)]
-  z[final_result == "", final_result := "std"]
-  rm_cols(z, .(result, order_result))
-  setorder(z, id, final_result)
-  z[grepl("dec", final_result, ignore.case = TRUE),
-    decision := "dec"]
-  z[is.na(decision) & grepl("na|uwer", final_result, ignore.case = TRUE),
-    decision := "uwer"]
-  z[is.na(decision) & grepl("excl", final_result, ignore.case = TRUE),
-    decision := pull_code_all("excl[0-9]\\([0-9\\-\\|\\[\\]]+\\)", final_result,
-                              collapse = ",", ignore.case = TRUE)]
+  z[, `:=`(final_result, sort_code(order_result)), .(id)]
+  z[grepl("dec", final_result, ignore.case = TRUE), `:=`(decision, "dec")]
+  z[is.na(decision) & grepl("na|uwer", final_result, ignore.case = TRUE), `:=`(decision, "uwer")]
+  z[is.na(decision) & grepl("excl|loading", final_result, ignore.case = TRUE),
+    `:=`(decision, pull_code_all("excl[0-9]\\([0-9\\-\\|\\[\\]]+\\)|loading\\([0-9]+\\)",
+                                 final_result, collapse = ",", ignore.case = TRUE))]
   set(z, i = which(is.na(z$decision)), j = "decision", value = "std")
-  set(z, j = "decision", value = toupper(z$decision))
   return(z[])
 }
 
