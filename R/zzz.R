42ee0fccf5f5a34cdc552db85ece9000f3bf0716
seokhoonj <seokhoonj@gmail.com>
2023-08-18 20:19:12 +0900

    `.onLoad` function is refined and `.onUnload` function is added

--- R/zzz.R
+++ R/zzz.R
@@ -1,15 +1,27 @@
 
 .onLoad <- function(libname, pkgname) {
-  packageStartupMessage("Written by Joo, Seokhoon. (Note, kcd terms arguments are monthly-basis)")
-  options(scipen = 14)
-  options(vuw.eps = 1e-8)
-  options(vuw.scipen = 14)
-  options(vuw.guess_max = 21474836)
-  options(vuw.two.colors1 = c("#80B1D3", "#FB8072"))
-  options(vuw.two.colors2 = c("#00BFC4", "#F8766D"))
-  options(vuw.two.colors3 = c("#377EB8", "#E41A1C"))
-  options(vuw.three.colors1 = c("#4DAF4A", "#377EB8", "#E41A1C"))
-  options(vuw.three.colors2 = c("#8DD3C7", "#80B1D3", "#FB8072"))
+  packageStartupMessage("Written by Seokhoon Joo. (Note, kcd terms arguments are monthly-basis)")
+  op <- options()
+  op.vuw <- list(
+    vuw.eps = 1e-8,
+    vuw.scipen = 14,
+    vuw.guess_max = 21474836,
+    vuw.two.colors1 = c("#80B1D3", "#FB8072"),
+    vuw.two.colors2 = c("#00BFC4", "#F8766D"),
+    vuw.two.colors3 = c("#377EB8", "#E41A1C"),
+    vuw.three.colors1 = c("#4DAF4A", "#377EB8", "#E41A1C"),
+    vuw.three.colors2 = c("#8DD3C7", "#80B1D3", "#FB8072")
+  )
+  toset <- !(names(op.vuw) %in% names(op))
+  if (any(toset)) options(op.vuw[toset])
+
   if (Sys.info()[["sysname"]] == "Linux")
     Sys.setlocale("LC_MESSAGES", "en_US.UTF-8")
+
+  invisible()
 }
+
+.onUnload <- function(libpath) {
+  library.dynam.unload("vuw", libpath)
+}
+
