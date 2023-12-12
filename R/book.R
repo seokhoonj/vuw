cf7c1caaca01d40509f19b86f7f4873ac2b88746
seokhoonj <seokhoonj@gmail.com>
2023-12-13 00:04:12 +0900

    size argument is modified in `amt_plot` function`

--- R/book.R
+++ R/book.R
@@ -247,7 +247,7 @@ amt_plot <- function(amt_mix, count.label = TRUE, prop.label = TRUE) {
   g1 <- ggbar(amt_mix_uni,
               x = age_band, y = nsum, ymax = ymax * 1.1,
               group = gender, fill = gender, label = label,
-              size = size, hjust = -.1) +
+              size = count.size, hjust = -.1) +
     scale_gender_manual(amt_mix_uni$gender) +
     scale_y_continuous(labels = comma) +
     facet_wrap(~ gender, ncol = 1) +
@@ -255,9 +255,12 @@ amt_plot <- function(amt_mix, count.label = TRUE, prop.label = TRUE) {
     coord_flip() +
     theme_test() +
     theme_view(x.size = 0, legend.position = "none")
-  g2 <- ggmix(amt_mix, x = age_band, y = prop, group = amount, fill = amount, label = label, size = size) +
+  g2 <- ggmix(amt_mix, x = age_band, y = prop, group = amount, fill = amount,
+              label = label, size = prop.size) +
     scale_fill_gradient(low = "#56B1F7", high = "#132B43",
-                        labels = function(x) str_pad(format(x, big.mark = ",", scientific = F), width = width)) +
+                        labels = function(x) str_pad(format(x, big.mark = ",",
+                                                            scientific = F),
+                                                     width = width)) +
     facet_wrap(~ gender, ncol = 1) +
     xlab("") +
     coord_flip() +
